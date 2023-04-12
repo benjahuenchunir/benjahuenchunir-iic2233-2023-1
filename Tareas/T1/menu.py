72f37898b72fc58fe29810f8c2dea59dc39cf7cb
Benjamin Huenchuñir <bahuenchunir@uc.cl>
2023-04-12 13:28:35 -0400

    Creada la funcion para Nueva Partida

--- Tareas/T1/menu.py
+++ Tareas/T1/menu.py
@@ -1,4 +1,6 @@
 import os
+import parametros
+import random
 
 
 def mostrar_menu_inicio():
@@ -10,13 +12,41 @@ def mostrar_menu_inicio():
         print("*** Menú de Inicio ***\n----------------------\n"
               "[1] Nueva partida\n[2] Cargar Partida\n[X] Salir\n")
         seleccion = input("Indique su opción (1, 2, o X):\n")
+        manejar_seleccion_menu_inicio(seleccion)
 
 
-def mostrar_menu_inicio():
+def manejar_seleccion_menu_inicio(seleccion: str):
+    if seleccion == "1":
+        with (open('arenas.csv', 'rt') as info_arenas,
+              open('excavadores.csv', 'rt') as info_excavadores):
+            arenas = ((arena.strip().split(',')
+                       for arena in info_arenas.readlines()[1:]))
+            arena_juego = random.choice(([arena for arena in arenas if arena[1]
+                                          == parametros.ARENA_INICIAL]))
+            total_excavadores = [(excavador.strip().split(',')
+                                  for excavador in info_excavadores.readlines()
+                                  )]
+            excavadores_juego = random.choices(
+                total_excavadores, k=parametros.CANTIDAD_EXCAVADORES_INICIALES)
+        mostrar_menu_principal(arena_juego, excavadores_juego)
+    elif seleccion == "2":
+        pass
+
+
+def mostrar_menu_principal(arena_juego: list, excavadores_juego: list):
     """
     Imprime el menu principal
     """
-    print("   *** Menú Principal ***\n--------------------------\n"
-          "[1] Simular día torneo\n[2] Ver estado torneo\n[3] Ver ítems\n"
-          "[4] Guardar partida\n [5] Volver\n [X] Salir del programa")
-    seleccion = input("Indique su opción (1, 2, 3, 4 5 o X):\n")
+    seleccion = ""
+    while seleccion != "X":
+        print("   *** Menú Principal ***\n--------------------------\n"
+              "[1] Simular día torneo\n[2] Ver estado torneo\n[3] Ver ítems\n"
+              "[4] Guardar partida\n [5] Volver\n [X] Salir del programa")
+        seleccion = input("Indique su opción (1, 2, 3, 4 5 o X):\n")
+
+
+def manejar_seleccion_menu_principal(seleccion: str):
+    pass
+
+
+mostrar_menu_inicio()
---
+++ Tareas/T1/parametros.py
@@ -0,0 +1,10 @@
+import random
+
+DIAS_TOTALES_TORNEO = 0
+ARENA_INICIAL = random.choice(['normal', 'mojada', 'rocosa', 'magnetica'])
+CANTIDAD_EXCAVADORES_INICIALES = random.randint(1, 8)  # TODO: Cambia el numero de excavadores?
+METROS_META = 0
+DIAS_TORNEO = 0
+PROB_ENCONTRAR_TESORO = 0
+PROB_ENCONTRAR_CONSUMIBLE = 0
+PROB_INICIAR_EVENTO = 0
