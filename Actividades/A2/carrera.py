75799f8c39663283b1f8df4182b500cfa68e5b84
Benjamin Huenchuñir <bahuenchunir@uc.cl>
2023-04-20 15:09:09 -0400

    Corredor listo

--- Actividades/A2/carrera.py
+++ Actividades/A2/carrera.py
@@ -3,31 +3,28 @@ from threading import Event, Lock, Thread
 from time import sleep
 
 
-# Completar
-class Corredor:
+class Corredor(Thread):
 
     TIEMPO_ESPERA = 0.5  # Tiempo entre avances del corredor
     PORCENTAJE_MIN = 70  # Mínimo avance del corredor
     PORCENTAJE_MAX = 100  # Máximo avance del corredor
     PROBABILIDAD_ROBAR = 0.3  # Probabilidad de robar la tortuga
 
-    def __init__(self, nombre: str, tortuga: Lock, 
+    def __init__(self, nombre: str, tortuga: Lock,
                  senal_inicio: Event, senal_fin: Event,
                  lock_verificar_tortuga: Lock) -> None:
         super().__init__(name=nombre)
         # Referencias al lock de la tortuga y las señales propias de la carrera
+        self.name = nombre
         self.lock_tortuga = tortuga
         self.senal_inicio = senal_inicio
         self.senal_fin = senal_fin
         self.lock_verificar_tortuga = lock_verificar_tortuga
-
         self.tiene_tortuga = False  # Booleano que indica si el corredor lleva la tortuga
         self.__posicion = 0
         self.__velocidad = 10
         self.__correr = True
-
-        # Completar
-        self.daemon = 'COMPLETAR'
+        self.daemon = True
 
     @property
     def posicion(self) -> float:
@@ -50,50 +47,62 @@ class Corredor:
         self.perder_tortuga()
 
     def avanzar(self) -> None:
-        # Completar
-        
+        self.posicion += randint(
+            self.PORCENTAJE_MIN * self.velocidad / 100,
+            self.PORCENTAJE_MAX * self.velocidad / 100)
         # Luego de avanzar impime su posición y duerme
         print(f'{self.name}: Avancé a {self.posicion:.2f}')
         sleep(self.TIEMPO_ESPERA)
 
     def intentar_capturar_tortuga(self) -> None:
         # Completar
+        if self.lock_tortuga.acquire(blocking=False):
+            self.tiene_tortuga = True
 
         # Si logra la captura, imprime un mensaje
         if self.tiene_tortuga:
             print(f'{self.name}: ¡Capturé la tortuga!')
 
     def perder_tortuga(self) -> None:
-        # Completar
-
+        self.tiene_tortuga = False
+        self.lock_tortuga.release()
         print(f'{self.name}: Perdí la tortuga :(')
-    
+
     def robar_tortuga(self) -> bool:
         # PROBABILIDAD_ROBAR de robar la tortuga
-        if random() < self.PROBABILIDAD_ROBAR:
-            # Completar
-
+        if random() < self.PROBABILIDAD_ROBAR:  # TODO revisar si falta algo
+            self.notificar_robo()  # TODO esto deberia resivir un rival
+            self.lock_tortuga.acquire()
+            self.tiene_tortuga = True
             print(f'{self.name}: ¡Robé la tortuga!')
-            return 'COMPLETAR'
+            return True
         else:
-            return 'COMPLETAR'
-            
+            return False
+
     def correr_primera_mitad(self):
         while self.posicion < 50:
-            # Completar
-            pass
+            self.avanzar()
 
     def correr_segunda_mitad(self) -> bool:
         while self.__correr:
-            # Completar
-            pass
+            with self.lock_verificar_tortuga:
+                if self.senal_fin.is_set():
+                    return False
+                elif self.posicion >= 100 and self.tiene_tortuga:
+                    self.senal_fin.set()
+                    self.lock_tortuga.release()
+                    return True
+                elif not self.tiene_tortuga:
+                    self.robar_tortuga()  # TODO esto no va entonces (porque dice que solo intenta robar una vez)
+                self.avanzar()
 
     def run(self) -> None:
-        # Completar
-        pass
+        self.senal_inicio.wait()
+        self.correr_primera_mitad()
+        self.intentar_capturar_tortuga()
+        self.correr_segunda_mitad()
 
 
-# Completar
 class Carrera:
     def __init__(self, corredor_1: Corredor, corredor_2: Corredor, 
                  senal_inicio: Event, senal_fin: Event) -> None:
@@ -107,11 +116,8 @@ class Carrera:
         self.corredor_2 = corredor_2
         corredor_1.asignar_rival(corredor_2.ser_notificado_por_robo)
         corredor_2.asignar_rival(corredor_1.ser_notificado_por_robo)
+        self.daemon = False
 
-        # Completar
-        self.daemon = 'COMPLETAR'
-    
-    # Completar
     def empezar(self) -> str:
         pass
 
