97955baf29180bc5dcc23224144eef18580e7aac
Benjamin Huenchuñir <bahuenchunir@uc.cl>
2023-06-15 15:58:25 -0400

    Todo bien

--- Actividades/A5/funciones.py
+++ Actividades/A5/funciones.py
@@ -76,26 +76,10 @@ class IteradorCarrito:
         return self
 
     def __next__(self):
-        print(self.iterable)
-        if self.productos_iterable is None:
+        if len(self.productos_iterable) == 0:
             raise StopIteration("Llegamos al final")
         else:
-            valor = self.productos_iterable
-            print(valor)
-            self.iterable = valor
+            self.productos_iterable.sort(key=lambda x: x.precio)
+            valor = self.productos_iterable[0]
+            self.productos_iterable.remove(valor)
             return valor
-
-if __name__ == '__main__':
-    def generador_producto():
-        yield Producto(1, "Arroz", 200, "Pasillo 1", 1000, "gr")
-        yield Producto(5, "Takoyaki", 100, "Pasillo 1", 1000, "ml")
-        yield Producto(17, "Mantequilla", 999, "Pasillo 2", 200, "gr")
-    
-    def test_IteradorCarrito_next():
-            productos = list(generador_producto())
-            carrito = IteradorCarrito(productos)
-            producto_1 = next(carrito)
-            producto_2 = next(carrito)
-            producto_3 = next(carrito)
-            
-    test_IteradorCarrito_next()
