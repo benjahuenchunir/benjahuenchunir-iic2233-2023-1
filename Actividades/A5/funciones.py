e513628674a9d3f79943b3f546ff5809d260a715
Benjamin Huenchuñir <bahuenchunir@uc.cl>
2023-06-15 15:49:17 -0400

    Bonus

--- Actividades/A5/funciones.py
+++ Actividades/A5/funciones.py
@@ -52,9 +52,8 @@ def filtrar_por_categoria(generador_productos: Generator,
     lista_ids = generador_a_lista(ids_productos)
     return filter(lambda x: x.id_producto in lista_ids, generador_productos)
 
-def agrupar_por_pasillo(generador_productos: Generator) -> Generator:
-    # TODO: Completar
-    pass
+def agrupar_por_pasillo(generador_productos: Generator) -> groupby:
+    return groupby(generador_productos, key=lambda x: x.pasillo)
 
 
 # ----------------------------------------------------------------------------
@@ -77,10 +76,26 @@ class IteradorCarrito:
         return self
 
     def __next__(self):
-        
-        if self.iterable is None:
+        print(self.iterable)
+        if self.productos_iterable is None:
             raise StopIteration("Llegamos al final")
-        else:           
-            valor = self.iterable
-            self.iterable = self.iterable.siguiente
+        else:
+            valor = self.productos_iterable
+            print(valor)
+            self.iterable = valor
             return valor
+
+if __name__ == '__main__':
+    def generador_producto():
+        yield Producto(1, "Arroz", 200, "Pasillo 1", 1000, "gr")
+        yield Producto(5, "Takoyaki", 100, "Pasillo 1", 1000, "ml")
+        yield Producto(17, "Mantequilla", 999, "Pasillo 2", 200, "gr")
+    
+    def test_IteradorCarrito_next():
+            productos = list(generador_producto())
+            carrito = IteradorCarrito(productos)
+            producto_1 = next(carrito)
+            producto_2 = next(carrito)
+            producto_3 = next(carrito)
+            
+    test_IteradorCarrito_next()
